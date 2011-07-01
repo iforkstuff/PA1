5fa4c62cfccaccf2c916c53c9c2b5a106b02d33a
Aria Arasteh <ariaarasteh@gmail.com>
2011-07-01 14:06:23 -0700

    20,000 pushes!

--- test_BST.cpp
+++ test_BST.cpp
@@ -12,110 +12,9 @@ using namespace std;
  */
 int main() {
 
-  /* Create an STL vector of some ints */
   vector<int> v;
-  v.push_back(3);
-  v.push_back(4);
-  v.push_back(1);
-  v.push_back(100);
-  v.push_back(-33);
-  v.push_back(1468);
-  v.push_back(446);
-  v.push_back(163);
-  v.push_back(701);
-  v.push_back(89);
-  v.push_back(1702);
-  v.push_back(1689);
-  v.push_back(949);
-  v.push_back(1828);
-  v.push_back(1730);
-  v.push_back(1556);
-  v.push_back(1613);
-  v.push_back(1096);
-  v.push_back(1804);
-  v.push_back(108);
-  v.push_back(506);
-  v.push_back(1264);
-  v.push_back(1484);
-  v.push_back(768);
-  v.push_back(1891);
-  v.push_back(1379);
-  v.push_back(1500);
-  v.push_back(815);
-  v.push_back(1332);
-  v.push_back(1336);
-  v.push_back(1864);
-  v.push_back(1418);
-  v.push_back(868);
-  v.push_back(847);
-  v.push_back(484);
-  v.push_back(262);
-  v.push_back(1104);
-  v.push_back(1129);
-  v.push_back(323);
-  v.push_back(478);
-  v.push_back(1580);
-  v.push_back(621);
-  v.push_back(812);
-  v.push_back(828);
-  v.push_back(1316);
-  v.push_back(866);
-  v.push_back(1036);
-  v.push_back(1411);
-  v.push_back(287);
-  v.push_back(683);
-  v.push_back(1726);
-  v.push_back(1042);
-  v.push_back(21);
-  v.push_back(1414);
-  v.push_back(437);
-  v.push_back(1249);
-  v.push_back(704);
-  v.push_back(1637);
-  v.push_back(-53);
-  v.push_back(1644);
-  v.push_back(735);
-  v.push_back(328);
-  v.push_back(783);
-  v.push_back(1003);
-  v.push_back(1586);
-  v.push_back(1465);
-  v.push_back(239);
-  v.push_back(376);
-  v.push_back(1651);
-  v.push_back(512);
-  v.push_back(740);
-  v.push_back(681);
-  v.push_back(1464);
-  v.push_back(1621);
-  v.push_back(1838);
-  v.push_back(411);
-  v.push_back(46);
-  v.push_back(1706);
-  v.push_back(34);
-  v.push_back(530);
-  v.push_back(477);
-  v.push_back(1428);
-  v.push_back(285);
-  v.push_back(407);
-  v.push_back(184);
-  v.push_back(979);
-  v.push_back(1110);
-  v.push_back(432);
-  v.push_back(741);
-  v.push_back(1837);
-  v.push_back(308);
-  v.push_back(1403);
-  v.push_back(1796);
-  v.push_back(1469);
-  v.push_back(1617);
-  v.push_back(1413);
-  v.push_back(1634);
-  v.push_back(1729);
-  v.push_back(1292);
-  v.push_back(1517);
-  v.push_back(883);
-
+  /* Create an STL vector of some ints */
+#include "inserts.txt"
 
   /* Create an instance of BST holding int */
   BST<int> b;
@@ -156,12 +55,13 @@ int main() {
   BST<int>::iterator en = b.end();
   BST<int>::iterator it = b.begin();
   for(; it != en; ++it) {
-    cout << *it << endl;
     if(*it != *vit) {
       cout << "Incorrect inorder iteration of BST." << endl;
       return -1;
-    }
+    } 
     ++vit;
   }
+  cout << "All elements matched sorted vector." << endl;
+
 
 }
